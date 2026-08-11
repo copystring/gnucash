983441d99b99d5c5e66d581a36663f658a823e01
copystring <1298861+copystring@users.noreply.github.com>
2026-08-11 19:00:07 +0200

    Make page setup asynchronous

--- gnucash/gnome-utils/print-session.c
+++ gnucash/gnome-utils/print-session.c
@@ -34,6 +34,79 @@ static GtkPageSetup *page_setup = NULL;
 G_LOCK_DEFINE_STATIC(print_settings);
 G_LOCK_DEFINE_STATIC(page_setup);
 
+typedef struct
+{
+    GtkPrintDialog *dialog;
+    GCancellable *cancellable;
+    GWeakRef parent;
+} PageSetupRequest;
+
+static void
+page_setup_request_parent_destroyed (gpointer user_data,
+                                     G_GNUC_UNUSED GObject *where_parent_was)
+{
+    PageSetupRequest *request = user_data;
+
+    g_cancellable_cancel (request->cancellable);
+}
+
+static void
+page_setup_request_free (PageSetupRequest *request)
+{
+    GtkWindow *parent = GTK_WINDOW (g_weak_ref_get (&request->parent));
+
+    if (parent)
+    {
+        g_object_weak_unref (G_OBJECT (parent),
+                             page_setup_request_parent_destroyed, request);
+        g_object_unref (parent);
+    }
+    g_weak_ref_clear (&request->parent);
+    g_clear_object (&request->cancellable);
+    g_clear_object (&request->dialog);
+    g_free (request);
+}
+
+static void
+page_setup_request_finished (GObject *source, GAsyncResult *result,
+                             gpointer user_data)
+{
+    PageSetupRequest *request = user_data;
+    GError *error = NULL;
+    GtkPrintSetup *setup =
+        gtk_print_dialog_setup_finish (GTK_PRINT_DIALOG (source), result,
+                                       &error);
+
+    if (setup)
+    {
+        GtkPrintSettings *settings =
+            gtk_print_setup_get_print_settings (setup);
+        GtkPageSetup *new_page_setup =
+            gtk_print_setup_get_page_setup (setup);
+
+        G_LOCK (print_settings);
+        g_clear_object (&print_settings);
+        if (settings)
+            print_settings = g_object_ref (settings);
+        G_UNLOCK (print_settings);
+
+        G_LOCK (page_setup);
+        g_clear_object (&page_setup);
+        if (new_page_setup)
+            page_setup = g_object_ref (new_page_setup);
+        G_UNLOCK (page_setup);
+
+        gtk_print_setup_unref (setup);
+    }
+    else if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+    {
+        g_warning ("Could not update print settings: %s",
+                   error ? error->message : "unknown error");
+    }
+    g_clear_error (&error);
+    page_setup_request_free (request);
+}
+
 
 void
 gnc_print_operation_save_print_settings(GtkPrintOperation *op)
@@ -70,39 +143,39 @@ gnc_print_operation_init(GtkPrintOperation *op, const gchar* jobname)
 void
 gnc_ui_page_setup(GtkWindow *parent)
 {
+    PageSetupRequest *request = g_new0 (PageSetupRequest, 1);
     GtkPrintSettings *settings = NULL;
-    GtkPageSetup *old_page_setup, *new_page_setup;
+    GtkPageSetup *current_page_setup = NULL;
 
-    /* Get a reference to the current print settings */
     G_LOCK(print_settings);
-    settings = print_settings;
-    if (settings)
-        g_object_ref(settings);
+    if (print_settings)
+        settings = g_object_ref (print_settings);
     G_UNLOCK(print_settings);
 
-    /* Get a reference to the current page setup */
-    G_LOCK(page_setup);
-    old_page_setup = page_setup;
-    if (old_page_setup)
-        g_object_ref(old_page_setup);
-    G_UNLOCK(page_setup);
-
-    /* Run dialog */
-    new_page_setup = gtk_print_run_page_setup_dialog(parent, old_page_setup,
-                     settings);
-
-    /* Save new page setup */
     G_LOCK(page_setup);
     if (page_setup)
-        g_object_unref(page_setup);
-    page_setup = new_page_setup;
+        current_page_setup = g_object_ref (page_setup);
     G_UNLOCK(page_setup);
 
-    /* Release references */
+    request->dialog = gtk_print_dialog_new ();
+    request->cancellable = g_cancellable_new ();
+    g_weak_ref_init (&request->parent, parent);
+    if (parent)
+        g_object_weak_ref (G_OBJECT (parent),
+                           page_setup_request_parent_destroyed, request);
+
+    gtk_print_dialog_set_title (request->dialog, _("Page Setup"));
+    gtk_print_dialog_set_modal (request->dialog, TRUE);
     if (settings)
-        g_object_unref(settings);
-    if (old_page_setup)
-        g_object_unref(old_page_setup);
+        gtk_print_dialog_set_print_settings (request->dialog, settings);
+    if (current_page_setup)
+        gtk_print_dialog_set_page_setup (request->dialog, current_page_setup);
+
+    gtk_print_dialog_setup (request->dialog, parent, request->cancellable,
+                            page_setup_request_finished, request);
+
+    g_clear_object (&settings);
+    g_clear_object (&current_page_setup);
 }
 
 GtkPrintSettings *gnc_print_get_settings()
